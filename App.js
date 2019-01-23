3285eb6a30008f4662ab83c1df981c2595f347c9
ms-kaneko <y.kaneko@miraistart.com>
2019-01-23 09:50:39 +0900

    [add]一覧画面

--- App.js
+++ App.js
@@ -1,22 +1,110 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
+import React, { Component } from 'react';
+import { StyleSheet, Image, View, ScrollView } from 'react-native';
+import { Container, Header, Left, Body, Right, Button, Icon, Title, Text, Footer, Content, Card, CardItem, DatePicker, Form, Item ,Label, Input, H1, H2, H3} from 'native-base';
+export default class GeneralExample extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>
-        Open up App.js to start working on your app!</Text>
-      </View>
+      <Container style={styles.container}>
+
+        <Content style={styles.content}>
+
+
+
+        <Form>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <Item style={styles.disp_half}>
+            <Input placeholder="地名" />
+          </Item>
+          <Item last style={styles.disp_half}>
+            <Input placeholder="フリーワード" />
+          </Item>
+        </View>
+        </Form>
+
+          <Button style={{flex: 1, flexDirection: 'row'}}>
+            <Text style={{width: "100%"}}>
+              検索
+          </Text>
+          </Button>
+
+      <ScrollView>
+        <Card>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+        <Image style={styles.store} source={require('./assets/shop.png')} />
+        <H1 style={styles.storename}>店舗名</H1>
+        </View>
+        <H3>店舗住所</H3>
+        <H2>店舗紹介</H2>
+        </Card>
+
+        <Card>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+        <Image style={styles.store} source={require('./assets/shop.png')} />
+        <H1 style={styles.storename}>店舗名</H1>
+        </View>
+        <H3>店舗住所</H3>
+        <H2>店舗紹介</H2>
+        </Card>
+
+        <Card>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+        <Image style={styles.store} source={require('./assets/shop.png')} />
+        <H1 style={styles.storename}>店舗名</H1>
+        </View>
+        <H3>店舗住所</H3>
+        <H2>店舗紹介</H2>
+        </Card>
+      </ScrollView>
+
+        </Content>
+
+        <Footer style={styles.footer}>
+        <Button style={styles.disp_half}>
+          <Text style={{justifyContent:'center',flexDirection:'row'}}>
+            一覧
+        </Text>
+        </Button>
+        <Button style={styles.disp_half}>
+          <Text style={{justifyContent:'center',flexDirection:'row'}}>
+            お気に入り
+        </Text>
+        </Button>
+        </Footer>
+      </Container>
+
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    height: "100%",
   },
+  content: {
+    padding: 30,
+  },
+  header: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  footer: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  store: {
+    resizeMode:'contain',
+        alignItems: "flex-start",
+        flex: 1,
+  },
+  storename: {
+    width: "50%",
+    alignItems: "flex-end",
+    flex: 1,
+  },
+  disp_half: {
+        width: "50%",
+        flex: 1,
+  },
+
 });
